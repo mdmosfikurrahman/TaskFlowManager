279ac986bd20621e2160e1556f4b3f6b22326191
Mosfik <mdmosfikurrahman.cse@gmail.com>
2025-06-26 15:36:50 +0600

    feat(graphql): integrate GraphQL support for departments
    - added GraphQL dependencies and configurations in build.gradle
    - created schema for departments with queries and mutations
    - implemented controller for GraphQL APIs handling departments
    - added exception handler for GraphQL-specific errors

---
+++ src/main/java/org/epde/tfm/infrastructure/exception/GraphQLExceptionHandler.java
@@ -0,0 +1,27 @@
+package org.epde.tfm.infrastructure.exception;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import io.micrometer.common.lang.NonNull;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphQLExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(@NonNull Throwable ex, @NonNull DataFetchingEnvironment env) {
+        if (ex instanceof NotFoundException notFoundException) {
+            return GraphqlErrorBuilder.newError(env)
+                    .message(notFoundException.getMessage())
+                    .errorType(graphql.ErrorType.DataFetchingException)
+                    .build();
+        }
+
+        return GraphqlErrorBuilder.newError(env)
+                .message("Unexpected error: " + ex.getMessage())
+                .errorType(graphql.ErrorType.DataFetchingException)
+                .build();
+    }
+}
